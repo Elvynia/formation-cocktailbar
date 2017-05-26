812bad27b89eb2c8c4456eabc821909f94d91d8d
Elvynia <elvynia@gmail.com>
2017-05-26 02:57:38 +0200

    Ajout de l'édition pour les cocktails avec le ModelAttribute de Spring. Impossible de faire marcher PathVariable en optionnel alors que ça devrait fonctionner depuis Spring 4.1...

--- src/main/java/fr/formation/cocktailbar/controller/CocktailController.java
+++ src/main/java/fr/formation/cocktailbar/controller/CocktailController.java
@@ -1,9 +1,12 @@
 package fr.formation.cocktailbar.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +18,8 @@ import fr.formation.cocktailbar.entity.Cocktail;
 @RequestMapping("/cocktail")
 public class CocktailController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(CocktailController.class);
+
 	@Autowired
 	private CocktailRepository repository;
 
@@ -25,14 +30,28 @@ public class CocktailController {
 		return mav;
 	}
 
+	@RequestMapping(path = "/edit/{id}")
+	public String showUpdate(final Model model, @PathVariable final Integer id) {
+		if (this.repository.exists(id)) {
+			model.addAttribute("cocktail", this.repository.findOne(id));
+			return "/cocktail/edit";
+		} else {
+			CocktailController.LOGGER
+					.warn("Trying to update a cocktail with a unknown id '{}' in database. Switching to creation.", id);
+			return this.showCreate(model);
+		}
+	}
+
 	@RequestMapping(path = "/edit", method = RequestMethod.GET)
-	public String newCocktail(final Model model) {
-		model.addAttribute("newCocktail", new Cocktail());
+	public String showCreate(final Model model) {
+		model.addAttribute("cocktail", new Cocktail());
 		return "cocktail/edit";
 	}
 
-	@RequestMapping(path = "edit", method = RequestMethod.POST)
-	public String createCocktail(@ModelAttribute("newCocktail") final Cocktail cocktail) {
+	@RequestMapping(path = "/edit", method = RequestMethod.POST)
+	public String createOrUpdate(@ModelAttribute final Cocktail cocktail) {
+		// L'identifiant étant déjà pré-rempli par Spring si il était présent dans la page, aucune condition à gérer
+		// pour distinguer la création de la mise à jour.
 		this.repository.save(cocktail);
 		return "redirect:/cocktail/";
 	}
--- src/main/webapp/WEB-INF/views/cocktail/edit.jsp
+++ src/main/webapp/WEB-INF/views/cocktail/edit.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -7,8 +8,11 @@
 <jsp:include page="../header.jsp" />
 <body>
 	<div class="container">
-		<h1>Créer un nouveau cocktail :</h1>
-		 <form:form action="#" method="post" modelAttribute="newCocktail">
+		<!-- Autre manière de gérer la condition du titre que pour le produit. -->
+		<h1>${not empty cocktail ? "Modifier un cocktail existant :" : "Créer un nouveau cocktail :" }</h1>
+		<c:url value="/cocktail/edit.html" var="postUrl" />
+		 <form:form action="${postUrl}" method="post" modelAttribute="cocktail">
+		 	<form:hidden path="id"/>
 		 	<div class="form-group">
 		 		<label for="name">Nom :</label>
 		 		<form:input path="name" id="name" class="form-control" />
