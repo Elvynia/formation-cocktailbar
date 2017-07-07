a595461de25be864c002f30a827e2ccf58ceed84
elvynia <elvynia@gmail.com>
2017-07-07 14:01:15 +0200

    Ajout des entités/DAO pour le role et le compte utilisateur. Ajout d'une page permettant d'effectuer des actions de type CRUD sur les roles.

---
+++ src/main/java/fr/formation/cocktailbar/entity/Account.java
@@ -0,0 +1,56 @@
+package fr.formation.cocktailbar.entity;
+
+public class Account {
+	
+	private Integer id;
+
+	private String username;
+	
+	private String password;
+	
+	private Role role;
+	
+	private Boolean enabled;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+	
+}
